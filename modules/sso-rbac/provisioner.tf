3e889c11c1f97bb926556dc6a6e35607cdbcdb9b
Vahagn Martirosyan <mianv@coursede.local>
2022-09-07 10:56:39 +0400

    Add sso-rbac module

---
+++ modules/sso-rbac/provisioner.tf
@@ -0,0 +1,20 @@
+data "aws_iam_roles" "permission_set_arns" {
+  for_each    = { for kr in local.role_binding : kr.name => kr }
+  path_prefix = "/aws-reserved/sso.amazonaws.com/eu-west-1/AWSReservedSSO_permission-set-${each.value.group}"
+}
+
+data "aws_iam_roles" "sso" {
+  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+}
+
+module "eks_auth" {
+  source = "aidanmelen/eks-auth/aws"
+  eks    = var.eks_module
+
+  map_roles = [for role_binding in local.role_binding : {
+    rolearn  = [for role_arn in module.permission_set_roles.arns_without_path : role_arn if length(regexall(".+AWSReservedSSO_permission-set-${role_binding.group}.+", role_arn)) > 0][0]
+    username = role_binding.group
+    groups   = [role_binding.group]
+    }
+  ]
+}
---
+++ modules/sso-rbac/variables.tf
@@ -0,0 +1,17 @@
+variable "assignment" {
+  type = list(object({
+    group     = string
+    namespace = string
+    name      = string
+
+    role = list(object({
+      actions   = list(string)
+      resources = list(string)
+
+    }))
+  }))
+}
+
+variable "eks_module" {
+  type = any
+}
