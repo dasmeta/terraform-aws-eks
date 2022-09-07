7da7b529e639bfff74b05b4fecca1d25155332a6
Karapetyan, Aram (ext.) <aram.karapetyan@proalpha.com>
2022-09-07 18:13:18 +0400

    chore: fix fluent-bit example

--- modules/fluent-bit/version.tf
+++ modules/fluent-bit/version.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "2.12.1"
