68eba7fa675c27259acf95fedef03c22ca99c88a
Vahagn Martirosyan <vahagn@Vahagns-MacBook-Pro.local>
2023-02-13 17:39:54 +0400

    feat(DMVP-1754): Remove unnecessary role creation

--- modules/autoscaler/main.tf
+++ modules/autoscaler/main.tf
@@ -1,14 +1,1 @@
 data "aws_region" "current" {}
-
-resource "aws_iam_role" "role" {
-  name = "cluster-autoscaler-${var.cluster_name}-${data.aws_region.current.name}"
-  assume_role_policy = templatefile("${path.module}/policies/trusted-policy.json", {
-    oidc           = var.cluster_oidc_arn,
-    current_region = data.aws_region.current.name,
-  oidc_id = local.oidc_id })
-  managed_policy_arns = [aws_iam_policy.policy.arn]
-}
-
-locals {
-  oidc_id = split("/", var.cluster_oidc_arn)[3]
-}
