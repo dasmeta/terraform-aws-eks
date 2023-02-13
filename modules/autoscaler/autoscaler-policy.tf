26d4f5c7c154c6cf57f88c2057385ee9ca5ae3c2
Vahagn Martirosyan <vahagn@Vahagns-MacBook-Pro.local>
2023-02-13 17:37:23 +0400

    feat(DMVP-1754): Add role creation

--- modules/autoscaler/autoscaler-policy.tf
+++ modules/autoscaler/autoscaler-policy.tf
@@ -5,3 +5,16 @@ resource "aws_iam_policy" "policy" {
 
   policy = file("${path.module}/policies/cluster-autoscaler-policy.json")
 }
+
+resource "aws_iam_role" "role" {
+  name = "cluster-autoscaler-${var.cluster_name}-${data.aws_region.current.name}"
+  assume_role_policy = templatefile("${path.module}/policies/trusted-policy.json", {
+    oidc           = var.cluster_oidc_arn,
+    current_region = data.aws_region.current.name,
+  oidc_id = local.oidc_id })
+  managed_policy_arns = [aws_iam_policy.policy.arn]
+}
+
+locals {
+  oidc_id = split("/", var.cluster_oidc_arn)[3]
+}
