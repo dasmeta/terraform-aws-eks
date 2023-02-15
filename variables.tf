4c80b8e858907f8ae7484890343a5f07bcef2f41
Vahagn Martirosyan <vahagn@Vahagns-MacBook-Pro.local>
2023-02-15 12:27:09 +0400

    feat(DMVP-2088): Update README.md also removed unnecessary variables

--- variables.tf
+++ variables.tf
@@ -4,49 +4,6 @@ variable "create" {
   description = "Whether to create cluster and other resources or not"
 }
 
-# VPC
-variable "vpc_name" {
-  type        = string
-  default     = null
-  description = "Creating VPC name."
-}
-
-variable "cidr" {
-  type        = string
-  default     = ""
-  description = "CIDR ip range."
-}
-
-variable "availability_zones" {
-  type        = list(string)
-  default     = null
-  description = "List of VPC availability zones, e.g. ['eu-west-1a', 'eu-west-1b', 'eu-west-1c']."
-}
-
-variable "private_subnets" {
-  type    = list(string)
-  default = null
-  # default = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
-  description = "Private subnets of VPC."
-}
-
-variable "public_subnets" {
-  type    = list(string)
-  default = null
-  # default = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
-  description = "Public subnets of VPC."
-}
-
-variable "public_subnet_tags" {
-  type    = map(any)
-  default = {}
-}
-
-variable "private_subnet_tags" {
-  type    = map(any)
-  default = {}
-}
-
 #EKS
 variable "cluster_name" {
   type        = string
@@ -270,12 +227,6 @@ variable "region" {
   type        = string
   default     = null
   description = "AWS Region name."
-}
-
-variable "vpc_id" {
-  description = "vpc id in which to create cluster, used when using custom VPC (e.g create_vpc is false)"
-  type        = string
-  default     = ""
 }
 
 variable "vpc" {
