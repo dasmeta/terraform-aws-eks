34aa9811d6a13969970ae9d476b25a96fc77a51c
Aram Karapetyan <aram@dasmeta.com>
2023-04-19 01:11:22 +0400

    fix(RD-1065-adot-module): Add ADOT template file

---
+++ modules/adot/tests/template_file/1-example.tf
@@ -0,0 +1,35 @@
+locals {
+  cluster_name = "eks"
+}
+resource "helm_release" "cert-manager" {
+  namespace        = "cert-manager"
+  create_namespace = true
+  name             = "cert-manager"
+  chart            = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  atomic           = true
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
+
+module "adot" {
+  source = "../../"
+
+  cluster_name                = local.cluster_name
+  eks_oidc_root_ca_thumbprint = "eks_oidc_root_ca_thumbprint"
+  oidc_provider_arn           = "oidc_provider_arn"
+  region                      = "eu-central-1"
+  adot_config = {
+    default_config = false
+    template = templatefile("${path.module}/templates/adot-values.yaml.tpl", {
+      region         = "eu-central-1"
+      cluster_name   = local.cluster_name
+      log_group_name = "/aws/containerinsights/${local.cluster_name}/adot"
+    })
+  }
+  depends_on = [
+    helm_release.cert-manager
+  ]
+}
