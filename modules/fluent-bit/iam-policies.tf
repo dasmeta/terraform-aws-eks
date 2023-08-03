5babbb194c8c9182c2b4a20f4f9d3a16823313fb
Aram Karapetyan <aram@dasmeta.com>
2023-08-03 13:45:25 +0400

    fix(RDGA-1356): Configure fluent-bit for send logs to s3

--- modules/fluent-bit/iam-policies.tf
+++ modules/fluent-bit/iam-policies.tf
@@ -2,38 +2,7 @@ resource "aws_iam_policy" "this" {
   name        = "${var.cluster_name}-fluent-bit"
   description = "Permissions that are required to manage AWS cloudwatch metrics by fluent bit"
 
-  policy = file("${path.module}/iam-policy.json")
-}
-
-resource "aws_iam_role" "fluent-bit" {
-  name = "${var.cluster_name}-fluent-bit"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    },
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "${var.oidc_provider_arn}"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "oidc.eks.${var.region}.amazonaws.com/id/${var.eks_oidc_root_ca_thumbprint}:aud": "sts.amazonaws.com"
-        }
-      }
-    }
-  ]
-}
-POLICY
+  policy = var.s3_permission ? file("${path.module}/iam-policy-s3-cloudwatch.json") : file("${path.module}/iam-policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "CloudWatchAgentServerPolicy" {
