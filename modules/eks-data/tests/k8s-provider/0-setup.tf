c97357a2b6b1c6e5c40ed15d4acbcad4333b523b
Aram Karapetyan <aram@dasmeta.com>
2023-08-08 10:47:45 +0400

    fix(DMVP-eks-data): Added submodule for eks-data

---
+++ modules/eks-data/tests/k8s-provider/0-setup.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    
+    test = {
+      source = "terraform.io/builtin/test"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+      configuration_aliases = []
+    }
+  
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
+      configuration_aliases = []
+    }
+  
+  }
+
+  required_version = ">= 1.3.0"
+}
+/**
+ * set the following env vars so that aws provider will get authenticated before apply:
+
+ export AWS_ACCESS_KEY_ID=xxxxxxxxxxxxxxxxxxxxxxxx
+ export AWS_SECRET_ACCESS_KEY=xxxxxxxxxxxxxxxxxxxxxxxx
+*/
+provider "aws" {
+  region = "eu-central-1"
+}
