37c1a41d7122412bf32c22c760d002ce094a91a5
Julia A <julia.agham@extern.corify.de>
2025-08-01 12:36:34 +0400

    fix(DMVP-vpc-id-fix): VPC ids

--- modules/aws-load-balancer-controller/variables.tf
+++ modules/aws-load-balancer-controller/variables.tf
@@ -43,6 +43,10 @@ variable "oidc_provider_arn" {
   type = string
 }
 
+variable "vpc_id" {
+  type = string
+}
+
 ## the load balancer access logs sync to s3=>lambda=>cloudwatch was disabled/commented-out so this params also need/can be commented,
 ## after then the fix be applied for enabling this functionality we can uncomment them
 
